1c77adfefd85cf22c5120cfddf66d6115e944d12
Rizwanul Islam Rudra <rizwanul.ru@chilindo.com>
2018-11-01 15:47:13 +0700

    first commit with html,css,js files

---
+++ script.js
@@ -0,0 +1,20 @@
+var xmlhttp = new XMLHttpRequest();
+var url = "https://jsonplaceholder.typicode.com/posts";
+
+xmlhttp.onreadystatechange = function() {
+    if(this.readyState == 4 && this.status == 200) {
+        var jsonArr = JSON.parse(this.responseText);
+        displayFunction(jsonArr);
+    }
+};
+xmlhttp.open("GET", url, false);
+xmlhttp.send();
+function displayFunction(arr) {
+    var result = "";
+    var pretable = '<tr>' + '<th>Post ID</th>' + '<th>Post Title</th>' +'<th>Description</th>' + '</tr>';
+    var i;                    
+    for(i=0; i<arr.length; i++) {
+        result += '<tr>' + '<td colspan="1">'+ arr[i].id+ '</td>'+ '<td>' + arr[i].title + '</td>' + '<td>' + arr[i].body + '</td>' + '</tr>';
+    }
+    document.getElementById("demo").innerHTML = pretable + result;
+}
